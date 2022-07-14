5ef215b29385ef2cef229766bf7795e24aec9ddd
Guthrie <71603173+tabiosg@users.noreply.github.com>
2022-07-14 17:22:03 -0400

    Add recalibrate by adjust forward code (#9)
    This code was ACTUALLY used in the demo.

--- micromouse/Core/Src/mouse.c
+++ micromouse/Core/Src/mouse.c
@@ -1,29 +1,66 @@
 #include "mouse.h"
 
+void recalibrate_by_adjust_forward()
+{
+	set_servo_angle(Front);
+	HAL_Delay(200);
+
+	float front_wall_cm = 20.0f;
+	// DONE TO MAKE SURE CORRECT DISTANCE FROM FRONT WALL
+	for (int i = 0; i < 5; ++i)
+	{
+		front_wall_cm = distance_of_object_in_cm();
+		float desired_front_distance_cm = 6.0f;
+		float front_wall_remainder_cm = front_wall_cm;
+		while (front_wall_remainder_cm > 18.0f)
+		{
+			front_wall_remainder_cm -= 18.0f;
+		}
+		float front_wall_error_cm = front_wall_remainder_cm - desired_front_distance_cm;  // positive number means we're too far away
+		float front_wall_error_in = front_wall_error_cm * 0.39;
+
+
+		if (front_wall_error_in > 0)
+		{
+			motors_forward(0.5f);
+			HAL_Delay(front_wall_error_in * 100.0f * 2.0f);
+			stop_all_motors();
+			HAL_Delay(300);
+		}
+		else
+		{
+			motors_backward(0.5f);
+			HAL_Delay(front_wall_error_in * -100.0f * 2.0f);
+			stop_all_motors();
+			HAL_Delay(300);
+		}
+
+	}
+}
+
 void recalibrate_by_turning()
 {
+	/*
 	set_servo_angle(Front);
 	int total_change = 0;
 	direction recalibrate_dir[6] = { Left, Right, Left, Right, Left, Right };
 	float min_cm_front = distance_of_object_in_cm();
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 2; ++i)
 	{
 		direction readjust_dir = recalibrate_dir[i];
-		for (int j = 0; j < 6; ++j)
+		for (int j = 0; j < 5; ++j)
 		{
+			if (requested_manual_command != AUTON_CHAR) return;
 			total_change += readjust_dir - 1;
-			if (total_change >= 10 || total_change <= -10)
+			if (total_change >= 4 || total_change <= -4)
 			{
-				return;
+				break;
 			}
-			if (requested_manual_command != AUTON_CHAR)
-			{
-				return 0;
-			}  // if (requested_manual_command != AUTON_CHAR)
+			if (requested_manual_command != AUTON_CHAR) return;
 			rotate_direction(readjust_dir, 0.5);
 			HAL_Delay(30);
 			stop_all_motors();
-			HAL_Delay(100);
+			HAL_Delay(150);
 			float current_cm_front = distance_of_object_in_cm();
 			if (current_cm_front < min_cm_front - 1)
 			{
@@ -33,7 +70,7 @@ void recalibrate_by_turning()
 			{
 				min_cm_front = current_cm_front;
 			}  // if (current_cm_front > max_cm_front)
-			else if (current_cm_front > min_cm_front * 1.000005)
+			else if (current_cm_front > min_cm_front)
 			{
 				break;  // if getting larger, then break
 			}  // else
@@ -43,35 +80,40 @@ void recalibrate_by_turning()
 	HAL_Delay(30);
 	stop_all_motors();
 	HAL_Delay(400);
+	*/
 
 }  // void recalibrate_by_turning()
 
 void calibrate_at_walls()
 {
+	/*
 	set_servo_angle(Left);
 	motors_backward(0.75);
-	HAL_Delay(150);
+	HAL_Delay(300);
 	stop_all_motors();
 	HAL_Delay(500);
+	if (requested_manual_command != AUTON_CHAR) return;
 	float distance_left_cm = distance_of_object_in_cm();
 	set_servo_angle(Right);
 	HAL_Delay(1000);
 	float distance_right_cm = distance_of_object_in_cm();
 	set_servo_angle(Front);
+	if (requested_manual_command != AUTON_CHAR) return;
 
-	if (distance_left_cm < 16 && distance_right_cm < 16)
+	if (distance_left_cm < WALL_DISTANCE_CM && distance_right_cm < WALL_DISTANCE_CM)
 	{
 		float distance_difference_right_left_cm = distance_right_cm - distance_left_cm;
-		int proposed_left_motor_mult = LEFT_MOTOR_MULT - distance_difference_right_left_cm * 10;
+		int proposed_left_motor_mult = LEFT_MOTOR_MULT - distance_difference_right_left_cm * MAX_MULT_CHANGE_RANGE;
 		// Subtract for the left motor multiplier because the left motor is actually the right one
 		LEFT_MOTOR_MULT = proposed_left_motor_mult < LEFT_MOTOR_MULT_DEFAULT - MAX_MULT_CHANGE_RANGE ? LEFT_MOTOR_MULT : proposed_left_motor_mult;
 		LEFT_MOTOR_MULT = proposed_left_motor_mult > LEFT_MOTOR_MULT_DEFAULT + MAX_MULT_CHANGE_RANGE ? LEFT_MOTOR_MULT : proposed_left_motor_mult;
 	}
 	motors_forward(0.75);
-	HAL_Delay(150);
+	HAL_Delay(300);
 	stop_all_motors();
 	HAL_Delay(400);
-
+	if (requested_manual_command != AUTON_CHAR) return;
+	*/
 }
 
 void rotate_direction_90(direction d)
@@ -85,18 +127,19 @@ void rotate_direction_90(direction d)
 		rotate_direction(Left, 1);
 		HAL_Delay(390);
 		stop_all_motors();
-		return;
+		break;;
 	case Front:
-		return;
+		break;
 	case Right:
 		rotate_direction(Right, 1);
 		HAL_Delay(400);
 		stop_all_motors();
-		return;
+		break;;
 	}  // switch(d)
 
 	// Done to try to adjust the robot.
 	recalibrate_by_turning();
+	recalibrate_by_adjust_forward();
 }  // rotate_direction_90(direction d)
 
 void rotate_180_degrees()
@@ -130,10 +173,12 @@ void rotate_360_degrees(direction d)
 
 void go_forward_one_unit()
 {
+//	recalibrate_by_adjust_forward();
 	motors_forward(1);
 	HAL_Delay(640);
 	stop_all_motors();
 	HAL_Delay(200);
 	calibrate_at_walls();
 	recalibrate_by_turning();
+	recalibrate_by_adjust_forward();
 }  // go_forward_one_unit()
